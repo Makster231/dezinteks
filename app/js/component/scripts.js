8ca8b75e88810d52ba2eef6959f545f688d597bd
Максим <makster559553@gmail.com>
2020-10-09 00:30:59 +0300

    compeled desktop version

---
+++ app/js/component/scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    document.body.style.opacity = "1";
+  },
+  false
+);
---
+++ app/js/scripts.js
@@ -0,0 +1,6 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.body.style.opacity = "1";
+}, false);
+//# sourceMappingURL=scripts.js.map
